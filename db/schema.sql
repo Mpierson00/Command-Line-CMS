678bfbe1bbf9ef4a7b1b4252fd072dfa7495fae0
Michael <mpierson00@gmail.com>
2024-04-27 22:53:35 -0400

    added table creation

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,17 @@ CREATE DATABASE project_db;
 
 USE project_db;
 
+DROP TABLE IF EXISTS employee, role, department;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
