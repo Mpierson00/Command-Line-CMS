6a6597481ed66c9d0109e6d870efec6b39854b85
Michael <mpierson00@gmail.com>
2024-04-28 19:00:34 -0400

    added insert in role

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,6 @@
+-- Insert data into 'department'
 INSERT INTO department (name) VALUES ('Sales'), ('Engineering'), ('Human Resources');
+
+-- Inserting data in 'role'
+INSERT INTO role (title, salary, department_id) VALUES ('Sales Lead', 50000, 1), ('Software Engineer', 80000, 2), ('HR Manager', 65000, 3);
 
