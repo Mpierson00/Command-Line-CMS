ec548e4e58c8b8bf74012cc7f96c49d4ca83dc68
Michael <mpierson00@gmail.com>
2024-04-28 18:49:31 -0400

    finished server functionality

--- server.js
+++ server.js
@@ -3,9 +3,28 @@ const express = require("express");
 const PORT = process.env.port || 3001;
 const app = express();
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(( req, res) => {
+app.use((req, res) => {
     res.status(404).end();
 });
+
+app.get('/', (req, res) => {
+    res.send('Employee Management API Running');
+});
+
+app.get('/api/departments', async (req, res) => {
+    try {
+        const departments = await getDepartments();
+        res.json(departments);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
