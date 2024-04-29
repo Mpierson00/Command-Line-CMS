16f77d55eda8da47ff60e72b05147a08497c8e2a
Michael <mpierson00@gmail.com>
2024-04-29 01:13:47 -0400

    finished setup configs to run the sql files

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const mysql = require('mysql12');
 
-const connection = mysql.createConnection({
+const connectionInfo = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
@@ -13,4 +13,4 @@ const connection = mysql.createConnection({
 });
 
 //export the connection to be used other places
-module.exports = connection.promise();
+module.exports = connectionInfo.promise();
--- db/setup.js
+++ db/setup.js
@@ -3,3 +3,29 @@ const fs = require('fs');
 const path = require('path');
 
 require('dotenv').config();
+
+async function runScript() {
+    const connection = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        multipleStatements: true
+    });
+
+    try {
+        // read and execute schema.sql
+        const schemaSql = fs.readFileSync(path.join(__dirname, 'schema.sql'), 'utf-8');
+        await connection.query(schemaSql);
+        console.log('Database schema has been set up.');
+
+        // read and execute seeds.sql
+        const seedsSql = fs.readFileSync(path.join(__dirname, 'seeds.sql'), 'utf-8');
+        await connection.query(seedsSql);
+        console.log('Database has been seeded.');
+        
+    } finally {
+        await connection.end();
+    }
+}
+
+runScript().catch(err => console.error(err));
